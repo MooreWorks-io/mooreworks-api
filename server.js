60337f45f06f1b20b6ffd6d2a18d03f32240e5d6
MooreWorks <AWrayM2001@gmail.com>
2025-05-23 21:26:02 -0500

    Added HTML route to serve email generator

--- server.js
+++ server.js
@@ -41,3 +41,8 @@ app.post('/generate', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'email-generator.html'));});
+  
