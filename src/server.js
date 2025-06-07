1e587ee2047b4ac0acf74f41a68dd63222f16329
MooreWorks <AWrayM2001@gmail.com>
2025-06-06 22:38:37 -0500

    new ejs code

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ dotenv.config();
 // Init + DB
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
