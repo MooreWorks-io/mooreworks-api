3985ffb4407391b96bf2bcc665ddf10486db4851
MooreWorks <AWrayM2001@gmail.com>
2025-06-06 23:23:00 -0500

    changed homepage style

--- src/public/index.js
+++ src/public/index.js
@@ -1,16 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const logoutBtn = document.getElementById('logoutBtn');
-  if (logoutBtn) {
-    logoutBtn.addEventListener('click', async () => {
-      const res = await fetch('/api/auth/logout', {
-        method: 'POST',
-        credentials: 'include'
-      });
-      if (res.ok) {
-        window.location.reload();
-      } else {
-        alert('Logout failed.');
-      }
-    });
-  }
-});
+//swapped to ejs
