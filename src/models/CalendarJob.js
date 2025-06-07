5522ea56ee94d1da4b9d6de85dc6ac0c5684913a
MooreWorks <AWrayM2001@gmail.com>
2025-06-06 21:37:56 -0500

    calaendar more

--- src/models/CalendarJob.js
+++ src/models/CalendarJob.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const CalendarJobSchema = new mongoose.Schema({
-  title: String,
+  jobType: String,
   date: String, // ISO format e.g. "2025-06-04"
   crew: String,
   address: String,
